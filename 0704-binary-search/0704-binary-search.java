2560bcc0281efd4fd817dfaa85f231a8aa08a5b3
omkarkangane2005 <omkarkangane1511@gmail.com>
2026-07-29 20:40:04 +0530

    Time: 0 ms (100%), Space: 48.3 MB (50.33%) - LeetHub

---
+++ 0704-binary-search/0704-binary-search.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int search(int[] nums, int target) {
+    int low = 0;
+    int high = nums.length-1;
+    int  result =-1;
+
+    while(low <= high){
+        int guess =  (low+high)/2;
+        if(nums[guess] < target){
+            low = guess+1;
+        }
+        else if(nums[guess] == target ){
+            return guess;
+        }
+        else{
+            high = guess - 1 ;
+        }
+    } 
+    return result ; 
+    }
+}
